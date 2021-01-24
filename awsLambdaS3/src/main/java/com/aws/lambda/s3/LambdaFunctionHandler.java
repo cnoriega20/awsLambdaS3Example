19a4914ec4a76a80eb38b58bde879f47b9ad8ce4
cnoriega20 <c79.noriega@gmail.com>
2021-01-24 15:30:48 -0600

    Refactor lambda function to get Product toString fron Json.

--- awsLambdaS3/src/main/java/com/aws/lambda/s3/LambdaFunctionHandler.java
+++ awsLambdaS3/src/main/java/com/aws/lambda/s3/LambdaFunctionHandler.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.runtime.Context;
@@ -23,53 +26,63 @@ public class LambdaFunctionHandler implements RequestHandler<Object, String> {
     public String handleRequest(Object input, Context context) {
         context.getLogger().log("Input: " + input);
 
-       Regions clientRegion = Regions.US_EAST_1;
-       String  bucket ="handy-inventory-bucket";
-       String key = "handy-tool-catalog.json";
-       S3Object s3Object = null;
-       String textContent = "";
-       
-       try {
-    	   AmazonS3 s3Client = AmazonS3ClientBuilder.standard().
-    			   withRegion(clientRegion).
-    			   build();
-    	   context.getLogger().log("Downloading an object");  
-    	   
-    	   // Get an object and print its contents.
-    	   s3Object = s3Client.getObject(new GetObjectRequest(bucket, key));
-    	   context.getLogger().log("Content-Type" + s3Object.getObjectMetadata().getContentType());
-    	   context.getLogger().log("Content: ");
-    	   
-    	   // Get an entire object, overriding the specified response headers, and print the object's content.
-    	   textContent = displayS3JsonString(s3Object.getObjectContent(), context);
-    	   context.getLogger().log("Line from text file: " + textContent); 
-    	   
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-        return textContent;
+       return processJsonData(context);
     }
+
+	public String processJsonData(Context context) {
+		Regions clientRegion = Regions.US_EAST_1;
+		   String  bucket ="handy-inventory-bucket";
+		   String key = "handy-tool-catalog.json";
+		   S3Object s3Object = null;
+		   String textContent = "";
+		   Product product = null;
+		   
+		   try {
+			   AmazonS3 s3Client = AmazonS3ClientBuilder.standard().
+					   withRegion(clientRegion).
+					   build();
+			   context.getLogger().log("Downloading an object");  
+			   
+			   // Get an object and print its contents.
+			   s3Object = s3Client.getObject(new GetObjectRequest(bucket, key));
+			   context.getLogger().log("Content-Type" + s3Object.getObjectMetadata().getContentType());
+			   context.getLogger().log("Content: ");
+			   
+			   // Get an entire object, overriding the specified response headers, and print the object's content.
+			   //textContent = displayS3JsonString(s3Object.getObjectContent(), context);
+			   product = getProductById(102, s3Object.getObjectContent(), context);
+			   context.getLogger().log("Line from text file: " + textContent); 
+			   
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		    return product.toString();
+	}
     
-    private static String displayS3JsonString(InputStream input, Context context) {
+    private Product getProductById(Integer id, InputStream input, Context context) throws IOException {
     	String outputStr = null;
     	Gson gson = new Gson();
     	Product[] products = null;
-    	
+    	Optional<Product> finalProduct = null;
     	try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(input));
+			
 			//Marshall Json data to an array of Products.
+			
 			products = gson.fromJson(br, Product[].class);
-			br.close();
+			List<Product> productList = Arrays.asList(products);
+			finalProduct = productList.stream().
+				    filter(prod -> prod.getId() == id).
+				    findFirst();		
+			
 		} catch (JsonIOException e) {
 			context.getLogger().log("Error: " + e.getMessage());
 			e.printStackTrace();
 		}catch(JsonSyntaxException ex) {
 			context.getLogger().log("Error: " + ex.getMessage());
 			ex.printStackTrace();
-		} catch (IOException e) {			
-			e.printStackTrace();
 		}
-    	return products[0].toString();
+    	return finalProduct.get();
     }
 
 }
